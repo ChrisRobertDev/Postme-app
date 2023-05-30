2497b04d888e2243555837e77897e67e338dd11e
ChrisRobertDev <cristi.dinur@gmail.com>
2023-05-30 21:04:34 +0300

    Creating and rendering posts

---
+++ postme-app/app/components/AddPost.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+
+export default function CreatePost() {
+  const [title, setTitle] = useState("");
+  const [isDisabled, setIsDisabled] = useState(false);
+  let toastPostID: string;
+
+  // Create a post
+  const { mutate } = useMutation(
+    async (title: string) => await axios.post("/api/posts/addPost", { title }),
+    {
+      onError: (err: any) => {
+        // console.log(err);
+        if (err instanceof axios.AxiosError) {
+          toast.error(err?.response?.data.message, { id: toastPostID });
+        }
+        setIsDisabled(false);
+      },
+      onSuccess: (data: any) => {
+        //console.log(data);
+        toast.success("Post has been made", { id: toastPostID });
+        setTitle("");
+        setIsDisabled(false);
+      },
+    }
+  );
+
+  const submitPost = async (e: React.FormEvent) => {
+    toastPostID = toast.loading("Creating your post", {
+      id: toastPostID,
+      duration: 1000,
+    });
+    e.preventDefault();
+    setIsDisabled(true);
+    mutate(title);
+  };
+
+  return (
+    <form onSubmit={submitPost} className="bg-white my-8 p-8 rounded-md">
+      <div className="flex flex-col my-4">
+        <textarea
+          onChange={(e) => setTitle(e.target.value)}
+          name="title"
+          value={title}
+          placeholder="What's on your mind?"
+          className="p-4 text-lg rounded-md my-2 bg-gray-200 border-l-4"
+        ></textarea>
+      </div>
+      <div className="flex items-center justify-between gap-2">
+        <p
+          className={`font-bold text-sm ${
+            title.length > 300 ? "text-red-700" : "text-gray-700"
+          }`}
+        >{`${title.length}/300`}</p>
+        <button
+          disabled={isDisabled}
+          className="text-sm bg-teal-600 text-white py-2 p-2 rounded-md"
+          type="submit"
+        >
+          Create a post
+        </button>
+      </div>
+    </form>
+  );
+}
